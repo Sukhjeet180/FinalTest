33004cb9259f5b6eb480b8b78fc6dbbf24ffc322
Sukhjeet180 <sukhjeetdhanoa180@gmail.com>
2020-11-19 19:53:58 -0500

    Final Test first Commit

---
+++ FinalTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Person[] persons=new Person[2];
+            persons[0] = new Adult("Sukhjeet","Kaur");
+            persons[1] = new Child("Moninder","Singh");
+
+            foreach(Person p in persons)
+            {
+                Console.WriteLine(p.FirstName +" "+p.LastName);
+            }
+
+        }
+   
+
+    }
+
+    public class Person
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        
+
+    }
+
+    public class Adult : Person
+    {
+        public override string FirstName { get; set; }
+        public Adult(string fname,string lname)
+        {
+            FirstName = fname;
+            LastName = lname;
+        }
+    }
+
+    public class Child : Person
+    {
+        public override string FirstName { get; set; }
+        public Child(string fname, string lname)
+        {
+            FirstName = fname;
+            LastName = lname;
+        }
+
+    }
+
+}
